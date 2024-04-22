b5a2189d97de507f90a9a4634f4fc1090f90b155
Queli Serrano <quelirenato34@gmail.com>
2024-04-22 15:18:00 +0100

    Refactor: Move inline function

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -17,11 +17,25 @@ type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
   const router = useRouter();
+  
   const {register, control, handleSubmit, formState: { errors }} = useForm<IssueForm>({
     resolver: zodResolver(createIssueSchema)
   });
+  
   const [error, setError] = useState(''); 
   const [isSubmitting, setSubmitting] = useState(false);
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+    } catch (error) {
+      setSubmitting(false);
+      setError('An unexpected error occurred.');
+    }
+    
+  }); 
 
   return (
     <div className='max-w-xl'>
@@ -31,17 +45,7 @@ const NewIssuePage = () => {
       </Callout.Root>}
       <form 
         className='space-y-3' 
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post('/api/issues', data);
-            router.push('/issues');
-          } catch (error) {
-            setSubmitting(false);
-            setError('An unexpected error occurred.');
-          }
-          
-        })}>
+        onSubmit={onSubmit}>
         <TextField.Root 
           placeholder="Title" {...register('title')}>
         </TextField.Root>
