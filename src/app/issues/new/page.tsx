1fce0461a4cb563456bbd8899af2bc254aa8b747
Queli Serrano <quelirenato34@gmail.com>
2024-04-22 15:09:17 +0100

    Added a spinner

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -8,9 +8,10 @@ import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from '@/app/ValidationSchemas';
+import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -20,6 +21,7 @@ const NewIssuePage = () => {
     resolver: zodResolver(createIssueSchema)
   });
   const [error, setError] = useState(''); 
+  const [isSubmitting, setSubmitting] = useState(false);
 
   return (
     <div className='max-w-xl'>
@@ -31,9 +33,11 @@ const NewIssuePage = () => {
         className='space-y-3' 
         onSubmit={handleSubmit(async (data) => {
           try {
+            setSubmitting(true);
             await axios.post('/api/issues', data);
             router.push('/issues');
           } catch (error) {
+            setSubmitting(false);
             setError('An unexpected error occurred.');
           }
           
@@ -48,7 +52,9 @@ const NewIssuePage = () => {
           render={({ field }) => <SimpleMDE placeholder="Description" {...field}/>}  
         />
         {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>
+          Submit New Issue {isSubmitting && <Spinner/>}
+        </Button>
       </form>
     </div>
   )
